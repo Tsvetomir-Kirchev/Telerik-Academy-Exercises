12765a1efe76c4420edad871bf4b8a77f2353101
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-02 16:25:41 +0300

    Added SquareOf12345 project

---
+++ C# Part 1/01.IntroToProgramming/SquareOf12345/SquareOf12345.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOf12345
+{
+    class SquareOf12345
+    {
+        static void Main(string[] args)
+        {
+            int number = 12345;
+            Console.WriteLine(number * number);
+        }
+    }
+}
