b93e4d0a88632379ad847a0bb181e44494051d6e
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-02 16:31:40 +0300

    Added PrintAgeAfter10Years prject

---
+++ C# Part 1/01.IntroToProgramming/PrintAgeAfter10Years/PrintAgeAfter10Years.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAgeAfter10Years
+{
+    class PrintAgeAfter10Years
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Age = ");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Age after 10 years: {0}", age + 10);
+        }
+    }
+}
