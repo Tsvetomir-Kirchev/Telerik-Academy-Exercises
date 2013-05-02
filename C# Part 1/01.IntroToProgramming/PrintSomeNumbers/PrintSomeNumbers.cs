a7debcf451471ed008794af7b5dc805ecb5132ce
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-02 16:07:20 +0300

    Added PrintSomeNumbers project

---
+++ C# Part 1/01.IntroToProgramming/PrintSomeNumbers/PrintSomeNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintSomeNumbers
+{
+    class PrintSomeNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(101);
+            Console.WriteLine(1001);
+        }
+    }
+}
