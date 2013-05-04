14f91799e686f81943a7b032ccb3de78a8f968d8
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 18:45:48 +0300

    Added 12.PrintASCIITable

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/12.PrintASCIITable/PrintASCIITable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.PrintASCIITable
+{
+    class PrintASCIITable
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 255; i++)
+            {
+                Console.WriteLine("{0} = {1}", i, (char)i);
+            }
+        }
+    }
+}
