1941d27b9bde1b62c90a7e4f606723772f3b0316
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 18:48:37 +0300

    Added 13.NullToIntAndDouble

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/13.NullToIntAndDouble/NullToIntAndDouble.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.NullToIntAndDouble
+{
+    class NullToIntAndDouble
+    {
+        static void Main(string[] args)
+        {
+            int? i = null;
+            double? d = null;
+            Console.WriteLine(i);
+            Console.WriteLine(d);
+        }
+    }
+}
