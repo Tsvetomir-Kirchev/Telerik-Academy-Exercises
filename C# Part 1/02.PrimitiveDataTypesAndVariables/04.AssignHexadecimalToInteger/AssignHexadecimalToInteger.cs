12fc6b844b4d74de05d6ce66a209c981635ba780
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 17:55:24 +0300

    Added 04.AssignHexadecimalToInteger

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/04.AssignHexadecimalToInteger/AssignHexadecimalToInteger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.AssignHexadecimalToInteger
+{
+    class AssignHexadecimalToInteger
+    {
+        static void Main(string[] args)
+        {
+            int number = 0xFE;
+            Console.WriteLine(number);
+        }
+    }
+}
