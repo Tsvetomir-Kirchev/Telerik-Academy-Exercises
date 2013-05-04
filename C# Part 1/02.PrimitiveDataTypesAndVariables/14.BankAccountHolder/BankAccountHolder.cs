e48f887cdfea7dea903048cad501ae93e597bebd
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 18:51:20 +0300

    Added 14.BankAccountHolder

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/14.BankAccountHolder/BankAccountHolder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.BankAccountHolder
+{
+    class BankAccountHolder
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string middleName;
+            string lastName;
+            decimal moneyBalance;
+            string bankName;
+            string IBAN;
+            string bicCode;
+            long firstCrediCardNumber;
+            long secondCreditCardNumber;
+            long thirdCreditCardNumber;
+        }
+    }
+}
