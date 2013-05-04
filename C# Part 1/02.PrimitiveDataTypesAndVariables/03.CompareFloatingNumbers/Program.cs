17b2bb58ffa87d115c2b6d1b9139af8aa3f3f6b2
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 17:52:18 +0300

    Added 03.COmpareFloatingNumbers

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/03.CompareFloatingNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.CompareFloatingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CompareFloatingPointNumbers(5.00000001, 5.00000003));
+            Console.WriteLine(CompareFloatingPointNumbers(5.3, 6.01));
+        }
+
+        public static bool CompareFloatingPointNumbers(double number1, double number2)
+        {
+            bool result;
+            if ((float)number1 == (float)number2)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
