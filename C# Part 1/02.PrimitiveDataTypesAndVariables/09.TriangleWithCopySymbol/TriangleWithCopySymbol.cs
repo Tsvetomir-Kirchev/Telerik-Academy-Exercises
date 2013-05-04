9f8f2ed0ed63ea3b29c5c64043827618532d6782
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 18:36:22 +0300

    Added 09.TriangleWithCopySymbol

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/09.TriangleWithCopySymbol/TriangleWithCopySymbol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.TriangleWithCopySymbol
+{
+    class TriangleWithCopySymbol
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            char copy = '\u00A9';
+            Console.WriteLine("   {0}", copy);
+            Console.WriteLine("  {0} {0}", copy);
+            Console.WriteLine(" {0}   {0}", copy);
+            Console.WriteLine("{0} {0} {0} {0}", copy);
+        }
+    }
+}
