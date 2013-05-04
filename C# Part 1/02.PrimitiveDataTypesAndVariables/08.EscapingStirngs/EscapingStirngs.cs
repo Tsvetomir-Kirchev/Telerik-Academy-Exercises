7dc5ab7fea938dec212a4dbf1d974e5edd9aef40
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 18:12:30 +0300

    Added 08.EscapingStrings

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/08.EscapingStirngs/EscapingStirngs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.EscapingStirngs
+{
+    class EscapingStirngs
+    {
+        static void Main(string[] args)
+        {
+            // Without escaping of the quotes give errors "The use of quoted strings causes difficulties."
+            string str = "The \"use\" of quoted strings causes difficulties.";
+            Console.WriteLine(str);
+        }
+    }
+}
