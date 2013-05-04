5d611599d7c4768b2080f47df0fa50af243719d2
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 17:10:13 +0300

    01.MostAppropriateDataType project added

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/01.MostAppropriateDataTypes/MostAppropriateDataTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.MostAppropriateDataTypes
+{
+    class MostAppropriateDataTypes
+    {
+        static void Main(string[] args)
+        {
+            ushort ush = 52130;
+            sbyte sb = -115;
+            int i = 4825932;
+            byte b = 97;
+            short sh = -10000;
+
+            Console.WriteLine("ushort: {0}", ush);
+            Console.WriteLine("sbyte: {0}", sh);
+            Console.WriteLine("int: {0}", i);
+            Console.WriteLine("byte: {0}", b);
+            Console.WriteLine("short: {0}", sh);
+        }
+    }
+}
