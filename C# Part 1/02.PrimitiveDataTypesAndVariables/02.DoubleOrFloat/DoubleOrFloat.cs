721fe8206542f1602aece851d7766ae718ba42d4
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 17:22:04 +0300

    Added 02.DoubleOrFloat project

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/02.DoubleOrFloat/DoubleOrFloat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.DoubleOrFloat
+{
+    class DoubleOrFloat
+    {
+        static void Main(string[] args)
+        {
+            double d = 34.567839023;
+            Console.WriteLine("{0} - double", d);
+            float f = 12.345f;
+            Console.WriteLine("{0} - float and double", f);
+            double d1 = 8923.1234857;
+            Console.WriteLine("{0} - double", d1);
+            double f1 = 3456.091;
+            Console.WriteLine("{0} - double and float", f1);
+        }
+    }
+}
