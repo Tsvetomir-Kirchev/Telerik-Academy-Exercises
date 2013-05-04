0f00a234e86b18900446af6edfff3663c6665f3c
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-04 18:42:30 +0300

    Aded 11.SwapValuesOfIntegers

---
+++ C# Part 1/02.PrimitiveDataTypesAndVariables/11.SwapValuesOfIntegers/SwapValuesOfIntegers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.SwapValuesOfIntegers
+{
+    class SwapValuesOfIntegers
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine("Before swapping");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+
+            a ^= b;
+            b ^= a;
+            a ^= b;
+
+            Console.WriteLine("After swapping");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+        }
+    }
+}
