60a81a10cb561d29acbf0fd5ae934381a50acf35
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-08 12:33:25 +0300

    Added FindThirdBit

---
+++ C# Part 1/03.OperatorsAndStatement/05.FindThirdBit/FindThirdBit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.FindThirdBit
+{
+    class FindThirdBit
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Int32.Parse(Console.ReadLine());
+            bool isOne = IsThirdBit1(number);
+
+            if (isOne)
+            {
+                Console.WriteLine("Third bit of {0} is 1", number);
+            }
+            else
+            {
+                Console.WriteLine("Third bit of {0} is 0", number);
+            }
+        }
+
+        public static bool IsThirdBit1(int number)
+        {
+            bool result = false;
+
+            int mask = 4;
+            int r = number & mask;
+
+            if (r != 0)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
