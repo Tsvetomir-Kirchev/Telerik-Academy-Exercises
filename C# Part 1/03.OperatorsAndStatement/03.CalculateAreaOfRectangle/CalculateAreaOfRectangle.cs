4842f9b1c5ff4ff9ddce580c0ea52e36266737bf
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-08 10:40:52 +0300

    Added CalculateAreaOfRectangle

---
+++ C# Part 1/03.OperatorsAndStatement/03.CalculateAreaOfRectangle/CalculateAreaOfRectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.CalculateAreaOfRectangle
+{
+    class CalculateAreaOfRectangle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("width = ");
+            int width = Int32.Parse(Console.ReadLine());
+            Console.Write("height = ");
+            int height = Int32.Parse(Console.ReadLine());
+
+            int area = CalculateArea(width, height);
+            Console.WriteLine("The area of rectangle with width = {0} and height = {1} is {2}",
+                width, height, area);
+        }
+
+        public static int CalculateArea(int width, int height)
+        {
+            return width * height;
+        }
+    }
+}
