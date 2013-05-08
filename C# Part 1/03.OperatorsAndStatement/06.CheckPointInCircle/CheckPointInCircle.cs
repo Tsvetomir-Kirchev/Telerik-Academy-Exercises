6b7dc0ffc8f3934e01b4f7f05b90b707bb5852c2
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-08 13:53:21 +0300

    Added CheckPointInCircle

---
+++ C# Part 1/03.OperatorsAndStatement/06.CheckPointInCircle/CheckPointInCircle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.CheckPointInCircle
+{
+    class CheckPointInCircle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = Int32.Parse(Console.ReadLine());
+
+            bool isInCircle = IsPointInCircle(x, y);
+            if (isInCircle)
+            {
+                Console.WriteLine("Point ({0}, {1}) is in circle 0, 5", x, y);
+            }
+            else
+            {
+                Console.WriteLine("Point ({0}, {1}) is not in circle 0, 5", x, y);
+            }
+        }
+
+        public static bool IsPointInCircle(int x, int y)
+        {
+            if ((x <= 5) && (y <= 5))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
