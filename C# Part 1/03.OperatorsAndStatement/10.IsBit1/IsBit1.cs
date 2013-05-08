c7c33c54e2612f2527cf3f2f5fdc5c58aff8dc61
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-08 15:14:50 +0300

    Added IsBit1

---
+++ C# Part 1/03.OperatorsAndStatement/10.IsBit1/IsBit1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.IsBit1
+{
+    class IsBit1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number = ");
+            int number = Int32.Parse(Console.ReadLine());
+            Console.Write("p = ");
+            int p = Int32.Parse(Console.ReadLine());
+
+            bool isBit1 = Is1(number, p);
+            Console.WriteLine(isBit1);
+        }
+
+        public static bool Is1(int number, int p)
+        {
+            bool result = false;
+
+            int mask = 1 << p;
+            int temp = number & mask;
+            if (temp != 0)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
