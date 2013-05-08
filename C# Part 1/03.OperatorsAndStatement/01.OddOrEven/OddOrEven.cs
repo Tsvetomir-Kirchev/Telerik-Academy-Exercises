d8789603abedde75653127ec983995d0ac2556a5
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-08 10:29:46 +0300

    Added OddOrEven

---
+++ C# Part 1/03.OperatorsAndStatement/01.OddOrEven/OddOrEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.OddOrEven
+{
+    class OddOrEven
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Number {0} is even", number);
+            }
+            else
+            {
+                Console.WriteLine("Number {0} is odd", number);
+            }
+        }
+    }
+}
