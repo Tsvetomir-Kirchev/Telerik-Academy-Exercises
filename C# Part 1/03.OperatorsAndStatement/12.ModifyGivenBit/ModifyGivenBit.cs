718fd64468f9626d970ec8ba4977e7b6b9bc908a
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-22 14:37:08 +0300

    Added ModifyGivenBit

---
+++ C# Part 1/03.OperatorsAndStatement/12.ModifyGivenBit/ModifyGivenBit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.ModifyGivenBit
+{
+    class ModifyGivenBit
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int number = Int32.Parse(Console.ReadLine());
+            Console.Write("v (0 or 1) = ");
+            int v = Int32.Parse(Console.ReadLine());
+            Console.Write("p = ");
+            int position = Int32.Parse(Console.ReadLine());
+
+            int modifiedNumber = ModifyBit(number, v, position);
+            Console.WriteLine("Number {0} after modified bit {1} with value {2} is {3}",
+                number, position, v, modifiedNumber);
+        }
+
+        private static int ModifyBit(int number, int v, int position)
+        {
+            int modifiedNumber = 0;
+
+            // TODO: Implement the logic
+
+            return modifiedNumber;
+        }
+    }
+}
