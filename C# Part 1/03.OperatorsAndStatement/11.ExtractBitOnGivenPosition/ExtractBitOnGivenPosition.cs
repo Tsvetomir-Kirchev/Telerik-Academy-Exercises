19a954c8cf30c1f05bc0e77d882936e8f682b336
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-22 13:54:40 +0300

    Added ExtractBitOnGivenPosition

---
+++ C# Part 1/03.OperatorsAndStatement/11.ExtractBitOnGivenPosition/ExtractBitOnGivenPosition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.ExtractBitOnGivenPosition
+{
+    class ExtractBitOnGivenPosition
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("i = ");
+            int number = Int32.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int bitPosition = Int32.Parse(Console.ReadLine());
+
+            int bitValue = GetValueOfBit(number, bitPosition);
+            Console.WriteLine("The value of bit {0} of number {1} is {2}",
+                bitPosition, number, bitValue);
+        }
+
+        private static int GetValueOfBit(int number, int bitPosition)
+        {
+            int bitValue = 0;
+            int mask = 1 << bitPosition;
+
+            int result = number & mask;
+            if (result != 0)
+            {
+                bitValue = 1;
+            }
+            else
+            {
+                bitValue = 0;
+            }
+
+            return bitValue;
+        }
+    }
+}
