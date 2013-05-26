530ce4a2afc751216ce865ffa1b450a73899c580
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-26 17:40:27 +0300

    Added PointInCircleAndOutOfRectangle

---
+++ C# Part 1/03.OperatorsAndStatement/09.PointInCircleAndOutOfRectangle/PointInCirleAndOutOfRectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.PointInCircleAndOutOfRectangle
+{
+    class PointInCirleAndOutOfRectangle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            double x = Double.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            double y = Double.Parse(Console.ReadLine());
+
+            if ((x > -1 && x < 5) && (y > -1 || y < 1))
+            {
+                Console.WriteLine("The point {0},{1} is inside the circle.", x, y);
+            }
+            else
+            {
+                Console.WriteLine("The point {0},{1} is not inside the circle.", x, y);
+            }
+        }
+    }
+}
