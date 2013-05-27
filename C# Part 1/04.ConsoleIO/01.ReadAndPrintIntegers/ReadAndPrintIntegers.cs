94f31a1bc1985b64c6b0461e53bc313a87799dd1
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-27 16:43:48 +0300

    Added ReadAndPrintIntegers

---
+++ C# Part 1/04.ConsoleIO/01.ReadAndPrintIntegers/ReadAndPrintIntegers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ReadAndPrintIntegers
+{
+    class ReadAndPrintIntegers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three numbers: ");
+            int firstNamber = Int32.Parse(Console.ReadLine());
+            int secondNumber = Int32.Parse(Console.ReadLine());
+            int thirdNumber = Int32.Parse(Console.ReadLine());
+
+            int sum = firstNamber + secondNumber + thirdNumber;
+            Console.WriteLine("The sum of the three numbers is {0}", sum);
+        }
+    }
+}
