f055dd7ad5abe289d6de6a03f830a688bc81070f
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-05-27 17:44:44 +0300

    Added CountOfNumbersWhichDevideBy5

---
+++ C# Part 1/04.ConsoleIO/04.CountOfNumberWhichDevideBy5/CountOfNumbersWhichDevideBy5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.CountOfNumberWhichDevideBy5
+{
+    class CountOfNumbersWhichDevideBy5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int firstNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int secondNumber = Int32.Parse(Console.ReadLine());
+
+            int count = GetCount(firstNumber, secondNumber);
+            Console.WriteLine("p({0}, {1}) = {2}", firstNumber, secondNumber, count);
+        }
+
+        private static int GetCount(int firstNumber, int secondNumber)
+        {
+            int count = 0;
+
+            if (secondNumber >= firstNumber)
+            {
+                for (int i = firstNumber; i <= secondNumber; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
