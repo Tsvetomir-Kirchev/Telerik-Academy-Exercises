83d31b16839413e2332074c658cc471867434367
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 17:50:11 +0300

    Added CalculateTrailingZerosOfNFactorial

---
+++ C# Part 1/06.Loops/13.CalculateTrailingZerosOfNFactorial/CalculateTrailingZerosOfNFactorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.CalculateTrailingZerosOfNFactorial
+{
+    class CalculateTrailingZerosOfNFactorial
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+            for (int i = n; i > 0; i--)
+            {
+                factorial *= i;
+            }
+
+            int zeros = 0;
+            for (int i = 5; i <= n; i *= 5)
+            {
+                zeros += n / i;
+            }
+            Console.WriteLine("Trailing zeros: {0}", zeros);
+        }
+    }
+}
