0420234833adfe78b5db8446a9d3232dc26a3785
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 15:26:47 +0300

    Added PrintGreaterNumber

---
+++ C# Part 1/04.ConsoleIO/05.PrintGreaterNumber/PrintGreaterNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.PrintGreaterNumber
+{
+    class PrintGreaterNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First number: ");
+            int firstNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Second number: ");
+            int secondNumber = Int32.Parse(Console.ReadLine());
+
+            int biggerNumber = GetBiggerNumber(firstNumber, secondNumber);
+            Console.WriteLine("The bigger number is: {0}", biggerNumber);
+        }
+
+        private static int GetBiggerNumber(int firstNumber, int secondNumber)
+        {
+            return firstNumber - ((firstNumber - secondNumber) & (firstNumber - secondNumber) >> 31);
+        }
+    }
+}
