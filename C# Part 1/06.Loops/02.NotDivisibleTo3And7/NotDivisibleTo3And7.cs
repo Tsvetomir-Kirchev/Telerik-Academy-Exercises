fdcdf4c1ed9c446514c8c9c7116158eaed12ffc3
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 16:55:03 +0300

    Added NotDivisibleTo3And7

---
+++ C# Part 1/06.Loops/02.NotDivisibleTo3And7/NotDivisibleTo3And7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.NotDivisibleTo3And7
+{
+    class NotDivisibleTo3And7
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i % 3 == 0) && (i % 7 == 0))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
