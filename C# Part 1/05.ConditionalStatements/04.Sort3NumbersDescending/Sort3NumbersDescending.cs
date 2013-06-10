0a0f8d135e55091c3eefe16acb28c533c3dd31ce
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 16:20:26 +0300

    Added Sort3NumbersDescending

---
+++ C# Part 1/05.ConditionalStatements/04.Sort3NumbersDescending/Sort3NumbersDescending.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Sort3NumbersDescending
+{
+    class Sort3NumbersDescending
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First Number = ");
+            int firstNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Second Number = ");
+            int secondNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Third number: ");
+            int thirdNumber = Int32.Parse(Console.ReadLine());
+
+
+            SortAndPrint(ref firstNumber, ref secondNumber, ref thirdNumber);
+        }
+
+        private static void SortAndPrint(ref int firstNumber, ref int secondNumber, ref int thirdNumber)
+        {
+            int highNumber;
+            int middleNumber;
+            int lowNumber;
+
+            if (firstNumber > secondNumber && firstNumber > thirdNumber)
+            {
+                highNumber = firstNumber;
+            }
+            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+            {
+                highNumber = secondNumber;
+            }
+            else
+            {
+                highNumber = thirdNumber;
+            }
+
+            if ((firstNumber < secondNumber && firstNumber > thirdNumber) ||
+                (firstNumber > secondNumber && firstNumber < thirdNumber))
+            {
+                middleNumber = firstNumber;
+            }
+            else if ((secondNumber < firstNumber && secondNumber > thirdNumber) ||
+                (secondNumber > firstNumber && secondNumber < thirdNumber))
+            {
+                middleNumber = secondNumber;
+            }
+            else
+            {
+                middleNumber = thirdNumber;
+            }
+
+            if (firstNumber < secondNumber && firstNumber < thirdNumber)
+            {
+                lowNumber = firstNumber;
+            }
+            else if (secondNumber < firstNumber && secondNumber < thirdNumber)
+            {
+                lowNumber = secondNumber;
+            }
+            else
+            {
+                lowNumber = thirdNumber;
+            }
+
+            firstNumber = highNumber;
+            secondNumber = middleNumber;
+            thirdNumber = lowNumber;
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            Console.WriteLine(thirdNumber);
+        }
+    }
+}
