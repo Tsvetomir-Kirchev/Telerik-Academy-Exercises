42ff050e84743339a9c0d96ac296614320589ec5
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 15:52:57 +0300

    Added ExchangeValueOfTwoIntegers

---
+++ C# Part 1/05.ConditionalStatements/01.ExchangeValueOfTwoIntegers/ExchangeValueOfTwoIntegers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ExchangeValueOfTwoIntegers
+{
+    class ExchangeValueOfTwoIntegers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("first number = ");
+            int firstNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("second number = ");
+            int secondNumber = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("first number: {0}, second number {1}", firstNumber, secondNumber);
+            if (firstNumber > secondNumber)
+            {
+                firstNumber ^= secondNumber;
+                secondNumber ^= firstNumber;
+                firstNumber ^= secondNumber;
+            }
+            Console.WriteLine("first number: {0}, second number {1}", firstNumber, secondNumber);
+        }
+    }
+}
