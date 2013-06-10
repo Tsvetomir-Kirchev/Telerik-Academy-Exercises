f2e12a7191ff8912998a67dea03cf184a797b73d
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 17:16:15 +0300

    Added CalculateSumOfNFactorialAndX

---
+++ C# Part 1/06.Loops/06.CalculateSumOfNFactorialAndX/CalculateSumOfNFactorialAndX.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.CalculateSumOfNFactorialAndX
+{
+    class CalculateSumOfNFactorialAndX
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("X = ");
+            int x = Int32.Parse(Console.ReadLine());
+
+            BigInteger s = 1;
+            BigInteger factorial = 1;
+            BigInteger xByX = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                xByX *= x;
+                factorial *= i;
+
+                s += (factorial / xByX);
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
