63289e4d90bbce4679b5585e18f28fda75421024
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 15:42:10 +0300

    Added Fibonacci

---
+++ C# Part 1/04.ConsoleIO/09.Fibonacci/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            PrintFirst100FibonacciSequence();
+        }
+
+        private static void PrintFirst100FibonacciSequence()
+        {
+            decimal previous = 0;
+            decimal result = 1;
+            decimal sum = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sum = result + previous;
+                previous = result;
+                result = sum;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
