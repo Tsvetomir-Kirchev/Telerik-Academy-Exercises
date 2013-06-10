4b66781873749235827d83427daaf3148ca430c2
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 16:51:28 +0300

    Added PrintNumbersFrom1ToN

---
+++ C# Part 1/06.Loops/01.PrintNumbersFrom1ToN/PrintNumbersFrom1ToN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.PrintNumbersFrom1ToN
+{
+    class PrintNumbersFrom1ToN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
