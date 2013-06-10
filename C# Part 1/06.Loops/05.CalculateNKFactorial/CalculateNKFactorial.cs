49d6da1d6f4d25a89f1cd2843d359d3a4df64bad
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-10 17:09:14 +0300

    Added CalculateNKFactorial

---
+++ C# Part 1/06.Loops/05.CalculateNKFactorial/CalculateNKFactorial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.CalculateNKFactorial
+{
+    class CalculateNKFactorial
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("K = ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            BigInteger nFactorial = 1;
+            for (int i = n; i > 0; i--)
+            {
+                nFactorial *= i;
+            }
+
+            BigInteger kFactorial = 1;
+            for (int i = k; i > 0; i--)
+            {
+                kFactorial++;
+            }
+
+            BigInteger nKFactorial = 1;
+            for (int i = (k - n); i > 0; i--)
+            {
+                nKFactorial *= i;
+            }
+
+            BigInteger result = (nFactorial * kFactorial) / nKFactorial;
+            Console.WriteLine(result);
+        }
+    }
+}
