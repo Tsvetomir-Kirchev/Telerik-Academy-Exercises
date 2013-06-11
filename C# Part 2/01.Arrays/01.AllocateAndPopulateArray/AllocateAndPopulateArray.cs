ff9bdd72afdf87ec0705b23dd6a040dd9c96d5e8
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-11 11:04:21 +0300

    Added AllocateAndPopulateArray

---
+++ C# Part 2/01.Arrays/01.AllocateAndPopulateArray/AllocateAndPopulateArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.AllocateAndPopulateArray
+{
+    class AllocateAndPopulateArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+
+            InitializeArray(arr);
+            PrintArray(arr);
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
+            }
+        }
+
+        private static void InitializeArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+            }
+        }
+    }
+}
