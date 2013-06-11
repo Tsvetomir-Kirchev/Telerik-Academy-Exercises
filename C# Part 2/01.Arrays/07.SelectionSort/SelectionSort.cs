0d2d69382b3975476bcc09a3d4c27b0b2f108622
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-11 12:31:24 +0300

    Added SelectionSort

---
+++ C# Part 2/01.Arrays/07.SelectionSort/SelectionSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, 2, 6, 0, 50, 12, -6, 90, 190 };
+
+            Sort(arr);
+            Print(arr);
+        }
+
+        private static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        public static void Sort(int[] arr)
+        {
+            int min = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = i;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                    int temp = arr[i];
+                    arr[i] = arr[min];
+                    arr[min] = temp;
+                }
+            }
+        }
+    }
+}
