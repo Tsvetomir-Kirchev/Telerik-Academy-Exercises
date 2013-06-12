6c25569a84ca791ab60521df369addabd73b0876
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-06-12 14:14:01 +0300

    Added BinarySearch

---
+++ C# Part 2/01.Arrays/11.BinarySearch/BinarySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.BinarySearch
+{
+    class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Element (between 0 and 1000) = ");
+            int element = Int32.Parse(Console.ReadLine());
+
+            int[] sortedArray = new int[1000];
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                sortedArray[i] = i;
+            }
+
+            int elementIndex = GetIndex(sortedArray, element, sortedArray[0], sortedArray.Length);
+            Console.WriteLine("The index of {0} is: {1}",
+                element, elementIndex);
+        }
+
+        private static int GetIndex(int[] arr, int element, int min, int max)
+        {
+            if (max < min)
+            {
+                return -1;
+            }
+            int middle = (min + max) / 2;
+
+            if (arr[middle] > element)
+            {
+                return GetIndex(arr, element, min, middle - 1);
+            }
+            else if (arr[middle] < element)
+            {
+                return GetIndex(arr, element, middle + 1, max);
+            }
+            else
+            {
+                return middle;
+            }
+        }
+    }
+}
