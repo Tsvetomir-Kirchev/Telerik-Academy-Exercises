c277217986ebe9c3e0d51349561bc3c84f8e93ed
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-01 18:23:17 +0300

    Added SortStringArrayByLength

---
+++ C# Part 2/02.MultidimensionalArrays/05.SortStringArrayByLength/SortStringArrayByLength.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.SortStringArrayByLength
+{
+    class SortStringArrayByLength
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "hi", "hello", "some", "text", "a", "interesting", "b" };
+
+            Sort(array);
+        }
+
+        static void Sort(string[] array)
+        {
+            var arrayAnscending =
+                from arr in array
+                orderby arr.Length
+                select arr;
+
+            foreach (var arr in arrayAnscending)
+            {
+                Console.WriteLine(arr);
+            }
+        }
+    }
+}
