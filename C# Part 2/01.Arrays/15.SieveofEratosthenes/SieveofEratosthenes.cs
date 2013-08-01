964d9c723fcf2122fdd7aacf1423f5eaa84a5604
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-01 13:28:32 +0300

    Added SieveOfEratosthenes

---
+++ C# Part 2/01.Arrays/15.SieveofEratosthenes/SieveofEratosthenes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.SieveofEratosthenes
+{
+    class SieveofEratosthenes
+    {
+        static void Main(string[] args)
+        {
+            int n = 10000000;
+            FindPrimeNumbers(n);
+        }
+
+        private static void FindPrimeNumbers(int n)
+        {
+            double biggestSquareRoot = Math.Sqrt(n);
+            bool[] eliminated = new bool[n];
+
+            List<long> primeList = new List<long>();
+            primeList.Add(2);
+            for (int i = 3; i <= n; i += 2)
+            {
+                if (!eliminated[i])
+                {
+                    if (i < biggestSquareRoot)
+                    {
+                        for (int j = i * i; j <= n; j += 2 * i)
+                        {
+                            eliminated[j] = true;
+                        }
+                    }
+                    primeList.Add(i);
+                }
+            }
+
+            foreach (var item in primeList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
