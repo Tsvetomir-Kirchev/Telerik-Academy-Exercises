42daa865119ff73bee1471f074a86eb44e02fd7e
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-03 14:46:47 +0300

    Added GetBiggerNumber

---
+++ C# Part 2/03.Methods/02.GetBiggerNumber/GetBiggerNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.GetBiggerNumber
+{
+    class GetBiggerNumber
+    {
+        static void Main(string[] args)
+        {
+            int max = GetMax(5, GetMax(4, 7));
+            Console.WriteLine(max);
+        }
+
+        public static int GetMax(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
