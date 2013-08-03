b04806fae1e916cdaed9c3d08b6844b58b15066b
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-03 15:20:13 +0300

    Added CheckTwoNeighboorsInArray

---
+++ C# Part 2/03.Methods/05.CheckTwoNeightboorsInArray/CheckTwoNeighboorsInArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.CheckTwoNeightboorsInArray
+{
+    class CheckTwoNeighboorsInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 4, 2, 3, 2, 7 };
+            bool isBigger = IsBiggerThanNeighboors(6, arr);
+            Console.WriteLine(isBigger);
+        }
+
+        public static bool IsBiggerThanNeighboors(int index, int[] arr)
+        {
+            bool isBigger = false;
+
+            if(index != 0 && index != arr.Length - 1)
+            {
+                if (arr[index] > arr[index - 1] && arr[index] > arr[index + 1])
+                {
+                    isBigger = true;
+                }
+            }
+            else if (index == 0 && index != arr.Length - 1)
+            {
+                if (arr[index] > arr[index + 1])
+                {
+                    isBigger = true;
+                }
+            }
+            else if(index != 0 && index == arr.Length - 1)
+            {
+                if(arr[index] > arr[index - 1])
+                {
+                    isBigger = true;
+                }
+            }
+
+            return isBigger;
+        }
+    }
+}
