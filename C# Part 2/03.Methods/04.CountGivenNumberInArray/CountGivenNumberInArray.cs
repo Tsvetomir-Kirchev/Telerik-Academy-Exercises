b830c6f56e5d27f80aa9f61803b176893b6bff45
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-03 15:07:24 +0300

    Added CountGivenNumberInArray

---
+++ C# Part 2/03.Methods/04.CountGivenNumberInArray/CountGivenNumberInArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.CountGivenNumberInArray
+{
+    class CountGivenNumberInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 2, 3, 4, 4, 2, 3, 3, 43, 32, 3 };
+            int count = GetCount(3, arr);
+            Console.WriteLine(count);
+        }
+
+        public static int GetCount(int number, int[] arr)
+        {
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == number)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
