e90dba9c5e1ac01104b650806cf99897ddabd269
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-03 17:59:19 +0300

    Added new sql statements from SqlGroupingAndDDL exercises

--- Databases/03.SqlGroupingAndDDL/statements.sql
+++ Databases/03.SqlGroupingAndDDL/statements.sql
@@ -23,5 +23,111 @@ WHERE Salary >
 -----------------------------------------------------------------------------------------------------
 --Write a SQL query to find the full name, salary and department of the employees that take the
 --minimal salary in their department. Use a nested SELECT statement.
-3
+SELECT 
+	FirstName, 
+	LastName, 
+	Salary, 
+	d.Name
+FROM Employees e
+	INNER JOIN Departments d
+		ON e.DepartmentId = d.DepartmentId
+WHERE Salary = (
+    SELECT MIN(Salary)
+    FROM Employees
+    WHERE DepartmentId = d.DepartmentId
+);
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find the average salary in the department #1.
+SELECT AVG(Salary) AS [Average Salary]
+FROM Employees
+WHERE DepartmentID = 1
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find the average salary  in the "Sales" department.
+SELECT AVG(Salary) AS [Average Salary]
+FROM Employees e
+	INNER JOIN Departments d
+		ON e.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Sales'
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find the number of employees in the "Sales" department.
+SELECT COUNT(*) AS [Number of Employees]
+FROM Employees e
+	INNER JOIN Departments d
+		ON e.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Sales'
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find the number of all employees that have manager.
+SELECT COUNT(*) AS [Number of employees with manager]
+FROM Employees
+WHERE ManagerID IS NOT NULL
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find the number of all employees that have no manager.
+SELECT COUNT(*) AS [Number of employees with manager]
+FROM Employees
+WHERE ManagerID IS NULL
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find all departments and the average salary for each of them.
+SELECT 
+	AVG(e.Salary) AS [Average salary],
+	d.Name
+FROM Employees e
+	INNER JOIN Departments d
+		ON e.DepartmentID = d.DepartmentID
+GROUP BY d.Name
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find the count of all employees in each department and for each town.
+SELECT 
+	COUNT(*) AS [Employees count],
+	d.Name,
+	t.Name
+FROM Employees e
+	INNER JOIN Departments d
+		ON e.DepartmentID = d.DepartmentID
+	INNER JOIN Addresses a
+		ON e.AddressID = a.AddressID
+	INNER JOIN Towns t
+		ON a.TownID = t.TownID
+GROUP BY d.Name, t.Name
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find all managers that have exactly 5 employees. 
+--Display their first name and last name.
+SELECT 
+	m.FirstName,
+	m.LastName
+FROM Employees e
+	INNER JOIN Employees m
+		ON e.ManagerID = m.EmployeeID
+GROUP BY m.EmployeeID, m.FirstName, m.LastName
+HAVING COUNT(*) = 5
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find all employees along with their managers. 
+--For employees that do not have manager display the value "(no manager)".
+SELECT
+	e.FirstName,
+	e.LastName,
+	COALESCE(m.FirstName + ' ' + m.LastName, 'No manager') AS [Manager]
+FROM Employees e
+	LEFT OUTER JOIN Employees m
+		ON e.ManagerID = m.EmployeeID
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to find the names of all employees whose last name is exactly 5 characters long. 
+--Use the built-in LEN(str) function.
+SELECT
+	FirstName,
+	LastName
+FROM Employees
+WHERE LEN(LastName) = 5
+-----------------------------------------------------------------------------------------------------
+--Write a SQL query to display the current date and time in the following format 
+--"day.month.year hour:minutes:seconds:milliseconds". 
+--Search in  Google to find how to format dates in SQL Server.
+SELECT CONVERT(VARCHAR(10), GETDATE(), 104) + ' ' + CONVERT(VARCHAR(13), GETDATE(), 114)
+-----------------------------------------------------------------------------------------------------
+--Write a SQL statement to create a table Users. Users should have username, password, 
+--full name and last login time. Choose appropriate data types for the table fields. 
+--Define a primary key column with a primary key constraint. 
+--Define the primary key column as identity to facilitate inserting records.
+--Define unique constraint to avoid repeating usernames. 
+--Define a check constraint to ensure the password is at least 5 characters long.
+
 -----------------------------------------------------------------------------------------------------
