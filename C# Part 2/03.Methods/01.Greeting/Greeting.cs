a3610c8215e6d7fba393d8e6b1b6d4ce804f4528
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-03 14:43:01 +0300

    Added Greeting

---
+++ C# Part 2/03.Methods/01.Greeting/Greeting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Greeting
+{
+    class Greeting
+    {
+        static void Main(string[] args)
+        {
+            SayHelloTo("Tsvetomir");
+        }
+
+        public static void SayHelloTo(string to)
+        {
+            Console.WriteLine("Hello, {0}", to);
+        }
+    }
+}
