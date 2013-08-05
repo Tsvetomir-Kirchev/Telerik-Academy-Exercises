b6394882ee52d2bcf6e4132617a64160a7ff6e95
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-05 13:58:38 +0300

    Added CalculateSum

---
+++ C# Part 2/05.ObjectsAndClasses/06.CalculateSum/CalculateSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.CalculateSum
+{
+    class CalculateSum
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers saparated by space: ");
+            string numberString = Console.ReadLine();
+
+            int sum = CalculateSumFromString(numberString);
+            Console.WriteLine("The sum of the nbumbers is: {0}", sum);
+        }
+
+        public static int CalculateSumFromString(string stringNumber)
+        {
+            string[] numbers = stringNumber.Split(' ');
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += (Int32.Parse(numbers[i]));
+            }
+
+            return sum;
+        }
+    }
+}
