909cce3d4f161bf3147a79fc3479821c95475aea
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-05 13:39:48 +0300

    Added CheckYearForLeap

---
+++ C# Part 2/05.ObjectsAndClasses/01.CheckYearForLeap/CheckYearForLeap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CheckYearForLeap
+{
+    class CheckYearForLeap
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an year: ");
+            int year = Int32.Parse(Console.ReadLine());
+
+            bool isLeap = DateTime.IsLeapYear(year);
+
+            if (isLeap)
+            {
+                Console.WriteLine("{0} year is leap.", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} year is not leap.", year);
+            }
+        }
+    }
+}
