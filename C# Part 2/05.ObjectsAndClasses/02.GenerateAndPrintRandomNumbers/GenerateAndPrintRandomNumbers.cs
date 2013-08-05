e84380ef6060b2804d5e07957ae03f4183fab691
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-05 13:41:43 +0300

    Added GenerateAndPrintRandomNumbers

---
+++ C# Part 2/05.ObjectsAndClasses/02.GenerateAndPrintRandomNumbers/GenerateAndPrintRandomNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.GenerateAndPrintRandomNumbers
+{
+    class GenerateAndPrintRandomNumbers
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomGenerator.Next(100, 200));
+            }
+        }
+    }
+}
