b4006a0c4542f5481161bc2451d04dd6655afe29
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-10 14:31:03 +0300

    Added CalculateAndPrintSquare

---
+++ C# Part 2/06.ExceptionsHandling/01.CalculateAndPrintSquare/CalculateAndPrintSquare.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CalculateAndPrintSquare
+{
+    class CalculateAndPrintSquare
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            try
+            {
+                int number = Int32.Parse(Console.ReadLine());
+                long square = Square(number);
+                Console.WriteLine("The square of {0} is {1}", number, square);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+
+        public static long Square(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return number * number;
+            }
+        }
+    }
+}
