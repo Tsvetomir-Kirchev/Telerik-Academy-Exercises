8ddab11e76e7350b4a0516ca844d43d173f5da22
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-10 22:00:24 +0300

    Added ReplaceOnlyHoleWords

---
+++ C# Part 2/07.TextFiles/08.ReplaceOnlyHoleWords/ReplaceOnlyHoleWords.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _08.ReplaceOnlyHoleWords
+{
+    class ReplaceOnlyHoleWords
+    {
+        static void Main(string[] args)
+        {
+            string start = "start";
+            string finish = "finish";
+            string pattern = @"\b" + start + @"\b";
+
+            string pathToRead = @"../../start.txt";
+            string pathToWrite = @"../../finish.txt";
+
+            try
+            {
+                StreamReader reader = new StreamReader(pathToRead);
+                using (reader)
+                {
+                    StreamWriter writer = new StreamWriter(pathToWrite);
+                    using (writer)
+                    {
+                        string line = reader.ReadLine();
+                        while (line != null)
+                        {
+                            string result = Regex.Replace(line, pattern, finish);
+                            writer.WriteLine(result);
+                            line = reader.ReadLine();
+                        }
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error while processing files.");
+            }
+        }
+    }
+}
