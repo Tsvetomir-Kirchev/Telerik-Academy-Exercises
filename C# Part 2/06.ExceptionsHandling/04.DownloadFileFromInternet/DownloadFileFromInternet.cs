8132b80479263257aa61b175307feab294b6c2ed
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-10 17:31:47 +0300

    Added DownloadFileFromInternet

---
+++ C# Part 2/06.ExceptionsHandling/04.DownloadFileFromInternet/DownloadFileFromInternet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.DownloadFileFromInternet
+{
+    class DownloadFileFromInternet
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string currentDurectory = Directory.GetCurrentDirectory();
+                WebClient client = new WebClient();
+                client.DownloadFile("http://www.devbg.org/img/Logo-BASD.jpg", currentDurectory + "\\myfile.jpg");
+            }
+            catch (WebException)
+            {
+
+                Console.WriteLine("WebException");
+            }
+            finally
+            {
+                Console.WriteLine("done");
+            }
+        }
+    }
+}
