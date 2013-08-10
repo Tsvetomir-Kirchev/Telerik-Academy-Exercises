f8b41d3237448b3a8e48c9aeceeec69a3b407664
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-10 21:43:01 +0300

    Added SortStrings

---
+++ C# Part 2/07.TextFiles/06.SortStrings/SortStrings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.SortStrings
+{
+    class SortStrings
+    {
+        static void Main(string[] args)
+        {
+            string path = @"../../people.txt";
+            string pathToWrite = @"../../sortedPeople.txt";
+            List<string> people = new List<string>();
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                using (reader)
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        people.Add(line);
+                        line = reader.ReadLine();
+                    }
+                }
+
+                StreamWriter writer = new StreamWriter(pathToWrite);
+                using (writer)
+                {
+                    people.Sort();
+                    foreach (string human in people)
+                    {
+                        writer.WriteLine(human);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error while processing the files.");
+            }
+        }
+    }
+}
