0da265843c68ff355f2ac20b4f0bb1109fc59b99
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-11 08:34:36 +0300

    Added ExtractTextFromXml

---
+++ C# Part 2/07.TextFiles/10.ExtractTextFromXml/ExtractTextFromXml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.ExtractTextFromXml
+{
+    class ExtractTextFromXml
+    {
+        static void Main(string[] args)
+        {
+            bool inTag = false;
+            string xml = null;
+            string path = @"../../xml.txt";
+
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                using (reader)
+                {
+                    xml = reader.ReadToEnd();
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error while processing file: {0}", path);
+            }
+
+            StringBuilder cleanText = ExtractText(ref inTag, xml);
+            Console.WriteLine(cleanText.ToString());
+        }
+
+        private static StringBuilder ExtractText(ref bool inTag, string xml)
+        {
+            StringBuilder cleanText = new StringBuilder();
+            for (int i = 0; i < xml.Length; i++)
+            {
+                if (xml[i] == '<')
+                {
+                    inTag = true;
+                    continue;
+                }
+                if (xml[i] == '>')
+                {
+                    inTag = false;
+                    continue;
+                }
+
+                if (inTag)
+                {
+                    continue;
+                }
+                else
+                {
+                    cleanText.Append(xml[i]);
+                }
+            }
+            return cleanText;
+        }
+    }
+}
