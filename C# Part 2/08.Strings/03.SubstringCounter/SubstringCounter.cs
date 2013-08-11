d73a198d59629bd1aa7e6a4b91488553f40e7574
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-11 11:57:56 +0300

    Added SubstringCounter

---
+++ C# Part 2/08.Strings/03.SubstringCounter/SubstringCounter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.SubstringCounter
+{
+    class SubstringCounter
+    {
+        static void Main(string[] args)
+        {
+            string text = "We are living in an yellow submarine. We don't have anything else. " + 
+                "Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.";
+            string target = "in";
+
+            int count = CountOccurences(text, target);
+            Console.WriteLine("{0} occures {1} times.",
+                target, count);
+        }
+
+        public static int CountOccurences(string text, string target)
+        {
+            int count = 0;
+            int index = -1;
+            while (true)
+            {
+                index++;
+                index = text.ToUpper().IndexOf(target.ToUpper(), index);
+                if (index == -1)
+                {
+                    break;
+                }
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
