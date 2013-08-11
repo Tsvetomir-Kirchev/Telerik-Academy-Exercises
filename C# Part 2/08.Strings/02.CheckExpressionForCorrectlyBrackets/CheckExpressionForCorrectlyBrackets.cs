f38f6e4eb401af227604f7dfdd33e0bd69bd0d40
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-11 11:14:18 +0300

    Added CheckExpressionForCorrectlyBrackets

---
+++ C# Part 2/08.Strings/02.CheckExpressionForCorrectlyBrackets/CheckExpressionForCorrectlyBrackets.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CheckExpressionForCorrectlyBrackets
+{
+    class CheckExpressionForCorrectlyBrackets
+    {
+        static void Main(string[] args)
+        {
+            string expression = "((a+b)/5-d)";
+            bool areCorrect = AreBracketsCorrect(expression);
+            Console.WriteLine("Are brackets of: {0} correct? {1}",
+                expression, areCorrect);
+        }
+
+        public static bool AreBracketsCorrect(string expression)
+        {
+            Stack<char> brackets = new Stack<char>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    brackets.Push('(');
+                }
+                if (expression[i] == ')')
+                {
+                    if (brackets.Count == 0)
+                    {
+                        return false;
+                    }
+                    brackets.Pop();
+                }
+            }
+
+            if (brackets.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
