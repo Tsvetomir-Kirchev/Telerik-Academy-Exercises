18c78d3000aea9933f75e56179fb5c19aefdac6a
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-11 11:07:21 +0300

    Added ReverseString

---
+++ C# Part 2/08.Strings/01.ReverseString/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ReverseString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string sample = "sample";
+            for (int i = sample.Length - 1; i >= 0; i--)
+            {
+                Console.Write(sample[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
