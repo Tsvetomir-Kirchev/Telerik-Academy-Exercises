7043e054575da6cf62f9eb078c449b17fd0f4369
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-12 14:02:14 +0300

    Added ReadString

---
+++ C# Part 2/08.Strings/05.ReadString/ReadString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.ReadString
+{
+    class ReadString
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string with maximum 20 characters: ");
+            string text = Console.ReadLine();
+
+            string str = FillStringWithStar(text);
+            Console.WriteLine(str);
+        }
+
+        public static string FillStringWithStar(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i > 20)
+                {
+                    sb.Append('*');
+                }
+                else
+                {
+                    sb.Append(text[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
