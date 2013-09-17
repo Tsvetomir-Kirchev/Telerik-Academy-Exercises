d39766c8851ac368ad88e26c836f58e574a65ded
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-17 11:30:48 +0300

    [Asp.Net Web Froms]: Added RandomNumberGenerator

---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/01.RandomNumberGenerator/default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="_01.RandomNumberGenerator._default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Random Number Generator</title>
+    </head>
+    <body>
+        <form id="formMain" runat="server">
+        <div>
+            <h1>Radnom Number Generator</h1>
+            Range from:
+            <input type="number" id="startNumber" runat="server" />
+            To:
+            <input type="number" id="endNumber" runat="server" />
+            <input type="button" id="btnGetNumber" runat="server" value="Get Random Number" 
+                onserverclick="btnGetNumber_ServerClick"/>
+            <div id="result" runat="server"></div>
+        </div>
+        </form>
+    </body>
+</html>
---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/01.RandomNumberGenerator/default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _01.RandomNumberGenerator
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnGetNumber_ServerClick(object sender, EventArgs e)
+        {
+            try
+            {
+                int firstNumber = Int32.Parse(this.startNumber.Value);
+                int seconNumber = Int32.Parse(this.endNumber.Value);
+                Random rand = new Random();
+                int randNumber = rand.Next(firstNumber, seconNumber);
+                this.result.InnerText = randNumber.ToString();
+            }
+            catch(FormatException)
+            {
+                this.result.InnerText = "Wrong Number Format!";
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                this.result.InnerText = "Second number must be greater than first!";
+            }
+        }
+    }
+}
