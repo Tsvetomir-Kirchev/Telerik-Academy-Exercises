e4aa674ee0dbf70312e75127e172430dfb0c6d07
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-17 11:44:35 +0300

    [Asp.Net Web Froms]: Added RandomNumberGeneratorWithWebControls

---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/02.RandomNumberGeneratorWebControls/default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="_02.RandomNumberGeneratorWebControls._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Random Number Generator</title>
+    </head>
+    <body>
+        <form id="formMain" runat="server">
+        <div>
+            <h1>Radnom Number Generator</h1>
+            Range from:
+            <asp:TextBox type="number" id="startNumber" runat="server" />
+            To:
+            <asp:TextBox type="number" id="endNumber" runat="server" />
+            <asp:Button type="button" id="btnGetNumber" runat="server" Text="Get Random Number" 
+                OnClick="btnGetNumber_ServerClick"/>
+            <br />
+            <asp:Label id="lbResult" runat="server"></asp:Label>
+        </div>
+        </form>
+    </body>
+</html>
---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/02.RandomNumberGeneratorWebControls/default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _02.RandomNumberGeneratorWebControls
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnGetNumber_ServerClick(object sender, EventArgs e)
+        {
+            try
+            {
+                int firstNumber = Int32.Parse(this.startNumber.Text);
+                int seconNumber = Int32.Parse(this.endNumber.Text);
+                Random rand = new Random();
+                int randNumber = rand.Next(firstNumber, seconNumber);
+                this.lbResult.Text = randNumber.ToString();
+            }
+            catch(FormatException)
+            {
+                this.lbResult.Text = "Wrong Number Format!";
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                this.lbResult.Text = "Second number must be greater than first!";
+            }
+        }
+    }
+}
