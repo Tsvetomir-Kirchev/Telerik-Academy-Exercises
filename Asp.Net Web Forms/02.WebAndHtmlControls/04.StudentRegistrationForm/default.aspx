68683e89a1c038832f63d4674f11a2d842ec514a
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-19 12:09:24 +0300

    [Asp.Net Web Forms]: Added StudentRegistrationForm

---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/04.StudentRegistrationForm/default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="_04.StudentRegistrationForm._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Registration Form</title>
+        <link href="Content/Site.css" rel="stylesheet" />
+    </head>
+    <body>
+        <form id="formMain" runat="server">
+        <div id="Registration" runat="server">
+            <fieldset>
+                <legend>Registration Form</legend>
+                <div>
+                    <span>First Name:</span>
+                    <asp:TextBox ID="TbFirstName" runat="server" />
+                </div>
+                <div>
+                    <span>Last Name:</span>
+                    <asp:TextBox ID="TbLastName" runat="server" />
+                </div>
+                <div>
+                    <span>Faculty Number:</span>
+                    <asp:TextBox ID="TbFacultyNumber" runat="server" />
+                </div>
+                <div>
+                    <span>University:</span>
+                    <asp:DropDownList ID="DropDownUniversities" runat="server">
+                        <asp:ListItem>FMI</asp:ListItem>
+                        <asp:ListItem>TU</asp:ListItem>
+                        <asp:ListItem>NBU</asp:ListItem>
+                        <asp:ListItem>Plovdiv FMI</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+                <div>
+                    <span>Speciality:</span>
+                    <asp:DropDownList ID="DropDownSpecialities" runat="server">
+                        <asp:ListItem>Informatics</asp:ListItem>
+                        <asp:ListItem>Software Engineering</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+                <div>
+                    <span>Courses:</span>
+                    <asp:ListBox ID="LbCourses" runat="server" SelectionMode="Multiple">
+                        <asp:ListItem>C# Programming</asp:ListItem>
+                        <asp:ListItem>Java Programming</asp:ListItem>
+                        <asp:ListItem>Mathematics</asp:ListItem>
+                        <asp:ListItem>DSA</asp:ListItem>
+                        <asp:ListItem>OOP</asp:ListItem>
+                    </asp:ListBox>
+                </div>
+                <asp:Button ID="BtnSubmit" runat="server" Text="Register"
+                    OnClick="BtnSubmit_Click" />
+            </fieldset>
+        </div>
+        </form>
+    </body>
+</html>
---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/04.StudentRegistrationForm/default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _04.StudentRegistrationForm
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            string firstName = this.TbFirstName.Text;
+            string lastName = this.TbLastName.Text;
+            string facultyNumber = this.TbFacultyNumber.Text;
+            string university = this.DropDownUniversities.SelectedValue;
+            string speciality = this.DropDownSpecialities.SelectedValue;
+            List<string> courses = new List<string>();
+
+            for (int i = 0; i < this.LbCourses.GetSelectedIndices().Length; i++)
+            {
+                courses.Add(this.LbCourses.Items[i].ToString());
+            }
+
+            CreateSummary(firstName, lastName, facultyNumber,
+                university, speciality, courses);
+        }
+
+        private void CreateSummary(string firstName, string lastName, 
+            string facultyNumber, string university, string speciality, List<string> courses)
+        {
+            this.Registration.Visible = false;
+
+            Panel panel = new Panel();
+            panel.ID = "Summary";
+            panel.Controls.Add(new LiteralControl("<h3>Summary:</h3>"));
+            panel.Controls.Add(new Label { Text = "First Name: " });
+            panel.Controls.Add(new Label { Text = firstName, CssClass = "float-right" });
+            panel.Controls.Add(new LiteralControl("<br />"));
+            panel.Controls.Add(new Label { Text = "Last Name: " });
+            panel.Controls.Add(new Label { Text = lastName, CssClass = "float-right" });
+            panel.Controls.Add(new LiteralControl("<br />"));
+            panel.Controls.Add(new Label { Text = "Faculty Number: " });
+            panel.Controls.Add(new Label { Text = facultyNumber, CssClass = "float-right" });
+            panel.Controls.Add(new LiteralControl("<br />"));
+            panel.Controls.Add(new Label { Text = "University: " });
+            panel.Controls.Add(new Label { Text = university, CssClass = "float-right" });
+            panel.Controls.Add(new LiteralControl("<br />"));
+            panel.Controls.Add(new Label { Text = "Speciality: " });
+            panel.Controls.Add(new Label { Text = speciality, CssClass = "float-right" });
+            panel.Controls.Add(new LiteralControl("<br />"));
+            panel.Controls.Add(new Label { Text = "Courses: " });
+            panel.Controls.Add(new LiteralControl("<br />"));
+            BulletedList ul = new BulletedList();
+            foreach (var course in courses)
+            {
+                ul.Items.Add(new ListItem(course));
+            }
+            panel.Controls.Add(ul);
+            this.formMain.Controls.Add(panel);
+        }
+
+    }
+}
