1de30b2773a1cd0d407a3d5f8c5f120d5774c798
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-20 12:25:43 +0300

    [Asp.Net Web Forms]: Added TicTacToe

---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/05.TicTacToe/default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="_05.TicTacToe._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Tic Tac Toe</title>
+        <link href="Content/Site.css" rel="stylesheet" />
+    </head>
+    <body>
+        <form id="formMain" runat="server">
+        <div id="game">
+            <h2>Scores:</h2>
+            <div id="scores">
+                <asp:Label ID="LbPlayerScores" runat="server"></asp:Label>
+                <asp:Label ID="LbComputerScores" runat="server"></asp:Label>
+            </div>
+            <asp:Button ID="Btn00" runat="server" OnClick="Btn_Click" />
+            <asp:Button ID="Btn01" runat="server" OnClick="Btn_Click" />
+            <asp:Button ID="Btn02" runat="server" OnClick="Btn_Click" />
+            <br />
+            <asp:Button ID="Btn10" runat="server" OnClick="Btn_Click" />
+            <asp:Button ID="Btn11" runat="server" OnClick="Btn_Click" />
+            <asp:Button ID="Btn12" runat="server" OnClick="Btn_Click" />
+            <br />
+            <asp:Button ID="Btn20" runat="server" OnClick="Btn_Click" />
+            <asp:Button ID="Btn21" runat="server" OnClick="Btn_Click" />
+            <asp:Button ID="Btn22" runat="server" OnClick="Btn_Click" />
+            <br />
+            <asp:Label ID="Winner" runat="server" Visible="false"></asp:Label>
+            <asp:Button ID="BtnStartNewGame" runat="server" Text="Start New Game"
+                OnClick="BtnStartNewGame_Click" Visible="false" />
+        </div>
+        </form>
+    </body>
+</html>
---
+++ Asp.Net Web Forms/02.WebAndHtmlControls/05.TicTacToe/default.aspx.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _05.TicTacToe
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        private static char[,] game;
+        private const char playerCharacter = 'X';
+        private const char computerCharacter = 'O';
+        private static List<Position> freePositions;
+        private static int playerScores;
+        private static int computerScores;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                playerScores = 0;
+                computerScores = 0;
+                InitGame();
+            }
+        }
+
+        private static void InitGame()
+        {
+            game = new char[3, 3];
+            for (int i = 0; i < game.GetLength(0); i++)
+            {
+                for (int j = 0; j < game.GetLength(1); j++)
+                {
+                    game[i, j] = '*';
+                }
+            }
+            freePositions = new List<Position>()
+                {
+                    new Position { X = 0, Y = 0},
+                    new Position { X = 0, Y = 1},
+                    new Position { X = 0, Y = 2},
+                    new Position { X = 1, Y = 0},
+                    new Position { X = 1, Y = 1},
+                    new Position { X = 1, Y = 2},
+                    new Position { X = 2, Y = 0},
+                    new Position { X = 2, Y = 1},
+                    new Position { X = 2, Y = 2}
+                };
+            Button btn = new Button();
+            Page page = new Page();
+            if (HttpContext.Current != null)
+            {
+                page = (Page)HttpContext.Current.Handler;
+                for (int i = 0; i < game.GetLength(0); i++)
+                {
+                    for (int j = 0; j < game.GetLength(1); j++)
+                    {
+                        btn = (Button)page.FindControl("Btn" + i + j);
+                        btn.Text = "";
+                        btn.CssClass = "defaultBtnStyle";
+                    }
+                }
+                Label lbPlayerScores = (Label)page.FindControl("LbPlayerScores");
+                lbPlayerScores.Text = "You: " + playerScores;
+                Label lbComputerScores = (Label)page.FindControl("LbComputerScores");
+                lbComputerScores.Text = "Computer: " + computerScores;
+                Label lbWinner = (Label)page.FindControl("Winner");
+                lbWinner.Text = "";
+                lbWinner.Visible = true;
+            }
+        }
+
+        protected void Btn_Click(object sender, EventArgs e)
+        {
+            string btnId = ((Button)sender).ID;
+            GetInput(btnId);
+        }
+
+        private static void GetInput(string btnId)
+        {
+            switch (btnId)
+            {
+                case "Btn00":
+                    PlayerMove(0, 0, btnId);
+                    break;
+                case "Btn01":
+                    PlayerMove(0, 1, btnId);
+                    break;
+                case "Btn02":
+                    PlayerMove(0, 2, btnId);
+                    break;
+                case "Btn10":
+                    PlayerMove(1, 0, btnId);
+                    break;
+                case "Btn11":
+                    PlayerMove(1, 1, btnId);
+                    break;
+                case "Btn12":
+                    PlayerMove(1, 2, btnId);
+                    break;
+                case "Btn20":
+                    PlayerMove(2, 0, btnId);
+                    break;
+                case "Btn21":
+                    PlayerMove(2, 1, btnId);
+                    break;
+                case "Btn22":
+                    PlayerMove(2, 2, btnId);
+                    break;
+            }
+        }
+
+        private static void PlayerMove(int p1, int p2, string btnId)
+        {
+            Button btn = new Button();
+            Page page = new Page();
+            if (HttpContext.Current != null)
+            {
+                page = (Page)HttpContext.Current.Handler;
+                btn = (Button)page.FindControl(btnId);
+            }
+            if (game[p1, p2] == '*')
+            {
+                game[p1, p2] = playerCharacter;
+                freePositions.Remove(new Position { X = p1, Y = p2 });
+                btn.Text = playerCharacter.ToString();
+                if (Win(playerCharacter))
+                {
+                    Label lbWinner = (Label)page.FindControl("Winner");
+                    lbWinner.Text = "You win!";
+                    lbWinner.Visible = true;
+                    playerScores++;
+                    return;
+                }
+                ComputerMove();
+            }
+        }
+
+        private static void HighlightWinBoxes(Position p1, Position p2, Position p3)
+        {
+            Button btn = new Button();
+            Page page = new Page();
+            if (HttpContext.Current != null)
+            {
+                page = (Page)HttpContext.Current.Handler;
+            }
+            btn = (Button)page.FindControl("Btn" + p1.X + p1.Y);
+            btn.CssClass = "higlightWinBoxes";
+            btn = (Button)page.FindControl("Btn" + p2.X + p2.Y);
+            btn.CssClass = "higlightWinBoxes";
+            btn = (Button)page.FindControl("Btn" + p3.X + p3.Y);
+            btn.CssClass = "higlightWinBoxes";
+        }
+
+        private static void ComputerMove()
+        {
+            Random rand = new Random();
+            int positionIndex = rand.Next(0, freePositions.Count);
+            Position position = freePositions[positionIndex];
+            int row = position.X;
+            int col = position.Y;
+
+            if (game[row, col] == '*')
+            {
+                game[row, col] = computerCharacter;
+                freePositions.Remove(position);
+                Button btn = new Button();
+                Page page = new Page();
+                if (HttpContext.Current != null)
+                {
+                    page = (Page)HttpContext.Current.Handler;
+                    btn = (Button)page.FindControl("Btn" + row + col);
+                    btn.Text = computerCharacter.ToString();
+                }
+                if (Win(computerCharacter))
+                {
+                    Label lbWinner = (Label)page.FindControl("Winner");
+                    lbWinner.Text = "You win!";
+                    lbWinner.Visible = true;
+                    computerScores++;
+                    return;
+                }
+            }
+            else
+            {
+                ComputerMove();
+            }
+        }
+
+        private static bool Win(char ch)
+        {
+            bool win = false;
+            if (game[0, 0] == ch && game[0, 1] == ch && game[0, 2] == ch)
+            {
+                HighlightWinBoxes(new Position(0, 0), new Position(0, 1), new Position(0, 2));
+                win = true;
+            }
+            if (game[1, 0] == ch && game[1, 1] == ch && game[1, 2] == ch)
+            {
+                HighlightWinBoxes(new Position(1, 0), new Position(1, 1), new Position(1, 2));
+                win = true;
+            }
+            if (game[2, 0] == ch && game[2, 1] == ch && game[2, 2] == ch)
+            {
+                HighlightWinBoxes(new Position(2, 0), new Position(2, 1), new Position(2, 2));
+                win = true;
+            }
+            if (game[0, 0] == ch && game[1, 0] == ch && game[2, 0] == ch)
+            {
+                HighlightWinBoxes(new Position(0, 0), new Position(1, 0), new Position(2, 0));
+                win = true;
+            }
+            if (game[0, 1] == ch && game[1, 1] == ch && game[2, 1] == ch)
+            {
+                HighlightWinBoxes(new Position(0, 1), new Position(1, 1), new Position(2, 1));
+                win = true;
+            }
+            if (game[0, 2] == ch && game[1, 2] == ch && game[2, 2] == ch)
+            {
+                HighlightWinBoxes(new Position(0, 2), new Position(1, 2), new Position(2, 2));
+                win = true;
+            }
+            if (game[0, 0] == ch && game[1, 1] == ch && game[2, 2] == ch)
+            {
+                HighlightWinBoxes(new Position(0, 0), new Position(1, 1), new Position(2, 2));
+                win = true;
+            }
+            if (game[1, 2] == ch && game[2, 1] == ch && game[0, 0] == ch)
+            {
+                HighlightWinBoxes(new Position(1, 2), new Position(2, 1), new Position(0, 0));
+                win = true;
+            }
+            if (win)
+            {
+                Button btn = new Button();
+                Page page = new Page();
+                if (HttpContext.Current != null)
+                {
+                    page = (Page)HttpContext.Current.Handler;
+                    btn = (Button)page.FindControl("BtnStartNewGame");
+                    btn.Visible = true;
+                }
+            }
+            return win;
+        }
+
+        protected void BtnStartNewGame_Click(object sender, EventArgs e)
+        {
+            InitGame();
+            this.BtnStartNewGame.Visible = false;
+        }
+    }
+}
