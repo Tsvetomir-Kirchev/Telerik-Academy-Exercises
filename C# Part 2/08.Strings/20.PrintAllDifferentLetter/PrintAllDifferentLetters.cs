72aeb375dde188ffd8b4c2da035d2dced490fecc
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-21 15:18:35 +0300

    [C# Part 2]: Added PrintAllDifferentLetters

---
+++ C# Part 2/08.Strings/20.PrintAllDifferentLetter/PrintAllDifferentLetters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.PrintAllDifferentLetter
+{
+    class PrintAllDifferentLetters
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+            int[] arr = new int[26];
+            foreach (var item in str)
+            {
+                char tempChar = Char.ToUpper(item);
+                if (tempChar >= 'A' && tempChar <= 'Z')
+                {
+                    int index = (int)(tempChar - 'A');
+                    arr[index]++;
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    char tempChar = (char)(i + 'A');
+                    Console.WriteLine("Letter {0} appears {1} time(s)", tempChar, arr[i]);
+                }
+            }
+        }
+    }
+}
