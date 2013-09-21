5ae075f15233c466f53f8e449893f0e44273feb1
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-21 14:19:49 +0300

    [C# Part 2]: Added ConvertStringToUnicodeChars

---
+++ C# Part 2/08.Strings/09.ConvertStringToUnicodeChars/ConvertStringToUnicodeChars.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.ConvertStringToUnicodeChars
+{
+    class ConvertStringToUnicodeChars
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a text: ");
+            string text = Console.ReadLine();
+
+            ConvertToUnicode(text);
+        }
+
+        static void ConvertToUnicode(string text)
+        {
+            char[] characters = text.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                ushort unicode = (ushort)characters[i];
+                Console.Write("\\u{0:x4}", unicode);
+            }
+            Console.WriteLine();
+        }
+    }
+}
