9f96669dc138191913252fb9d5ff0434e7ed4eac
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-21 14:16:32 +0300

    [C# Part 2]: Added ReplaceForbiddenWords

---
+++ C# Part 2/08.Strings/08.ReplaceForbiddenWords/ReplaceForbiddenWords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.ReplaceForbiddenWords
+{
+    class ReplaceForbiddenWords
+    {
+        static void Main(string[] args)
+        {
+            string[] forbiddenWords = { "Microsoft", "PHP", "CLR" };
+            string text = "Microsoft announced its next generation PHP compiler today. " +
+                          "It is based on .NET Framework 4.0 and is implemented as a dynamic language in CLR." +
+                          "PHP is not cool.";
+
+            string censoredText = ReturnCensoredText(text, forbiddenWords);
+            Console.WriteLine(censoredText);
+        }
+
+        static string ReturnCensoredText(string text, string[] forbiddenWords)
+        {
+            StringBuilder stars = new StringBuilder();
+            for (int i = 0; i < forbiddenWords.Length; i++)
+            {
+                stars.Clear();
+                for (int j = 0; j < forbiddenWords[i].Length; j++)
+                {
+                    stars.Append('*');
+                }
+                text = text.Replace(forbiddenWords[i], stars.ToString());
+            }
+
+            return text;
+        }
+    }
+}
