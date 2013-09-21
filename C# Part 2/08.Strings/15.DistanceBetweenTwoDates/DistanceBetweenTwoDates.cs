dc5a6ea3eb9874e104997bb8947edbdfb0cf09b6
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-21 15:06:23 +0300

    [C# Part 2]: Added DistanceBetweenTwoDates

---
+++ C# Part 2/08.Strings/15.DistanceBetweenTwoDates/DistanceBetweenTwoDates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _15.DistanceBetweenTwoDates
+{
+    class DistanceBetweenTwoDates
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("bg-BG");
+            Console.Write("Enter the first date (day.month.year): ");
+            DateTime firstDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Enter the second date (day.month.year): ");
+            DateTime secondDate = DateTime.Parse(Console.ReadLine());
+            
+            int distance = secondDate.Day - firstDate.Day;
+
+            Console.WriteLine("Distance: {0} days", distance);
+        }
+    }
+}
