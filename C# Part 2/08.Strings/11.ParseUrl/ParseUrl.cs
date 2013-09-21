4770d4f5e008827bc2c87f91c0bca408716c9538
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-21 14:52:57 +0300

    [C# Part 2]: Added ParseUrl

---
+++ C# Part 2/08.Strings/11.ParseUrl/ParseUrl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.ParseUrl
+{
+    class ParseUrl
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter url address: ");
+                string urlAddress = Console.ReadLine();
+                urlAddress += "/";
+
+                int protocolEndIndex = urlAddress.IndexOf("://");
+                string protocol = urlAddress.Substring(0, protocolEndIndex);
+
+                int serverFirstIndex = protocolEndIndex + 3;
+                int serverLastIndex = urlAddress.IndexOf("/", serverFirstIndex);
+                string server = urlAddress.Substring(serverFirstIndex, serverLastIndex - serverFirstIndex);
+
+                string resource = null;
+                if (serverLastIndex + 1 < urlAddress.Length)
+                {
+                    resource = urlAddress.Substring(serverLastIndex + 1, urlAddress.Length - serverLastIndex - 2);
+                }
+
+                Console.WriteLine("[protocol] = " + protocol);
+                Console.WriteLine("[server] = " + server);
+                if (resource != null)
+                {
+                    Console.WriteLine("[resource] = " + resource);
+                }
+                else
+                {
+                    Console.WriteLine("[resurce] = No");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The format must be for example [http://domain.com/reosurce]");
+            }
+        }
+    }
+}
