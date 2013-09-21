051639219804a30e0e25b5fad59073bef2042eb3
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-21 14:09:29 +0300

    [C# Part 2]: Added EncodeDecodeStrings

---
+++ C# Part 2/08.Strings/06.EncodeDecodeStrings/EncodeDecodeStrings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.EncodeDecodeStrings
+{
+    class EncodeDecodeStrings
+    {
+        static void Main(string[] args)
+        {
+            string cipher = "abc";
+            string text = "Some string";
+
+            string encode = EncodeDecode(text, cipher);
+            Console.WriteLine("Encoded: {0}", encode);
+            string decode = EncodeDecode(encode, cipher);
+            Console.WriteLine("Decoded: {0}", decode);
+        }
+
+        static string EncodeDecode(string text, string cipher)
+        {
+            Queue<char> cipherQueue = new Queue<char>(cipher);
+            StringBuilder output = new StringBuilder();
+
+            char code;
+            foreach (char item in text)
+            {
+                code = cipherQueue.Dequeue();
+                cipherQueue.Enqueue(code);
+                output.Append((char)(item ^ code));
+            }
+            return output.ToString();
+        }
+    }
+}
