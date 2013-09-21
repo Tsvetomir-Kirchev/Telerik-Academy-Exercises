ce0d035f7c1b42d23f061a6ee6f6cabdc6dbe1b1
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-09-21 14:55:00 +0300

    [C# Part 2]: Added ReverseWordsInSentence

---
+++ C# Part 2/08.Strings/12.ReverseWordsInSentence/ReverseWordsInSentence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.ReverseWordsInSentence
+{
+    class ReverseWordsInSentence
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "C# is not C++, not PHP and not Delphi!";
+
+            string reversedWords = ReverseWords(sentence);
+            Console.WriteLine(reversedWords);
+        }
+
+        static string ReverseWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            int index = words.Length;
+            string[] reversedWords = new string[index];
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < index; i++)
+            {
+                reversedWords[index - i - 1] = words[i];
+                sb.Append(words[index - i - 1]);
+                if (i != (index - 1))
+                {
+                    sb.Append(" ");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
