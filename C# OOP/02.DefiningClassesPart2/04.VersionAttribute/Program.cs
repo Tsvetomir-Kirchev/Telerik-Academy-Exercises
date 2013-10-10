969a8ce8edd7e52db09cbfb926f4a95ab1905238
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-10-10 21:02:13 +0300

    [C# OOP]: Added VersionAttribute

---
+++ C# OOP/02.DefiningClassesPart2/04.VersionAttribute/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.VersionAttribute
+{
+    [Version("1.2")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Program);
+            object[] versionAttributes = type.GetCustomAttributes(false);
+            VersionAttribute attr = versionAttributes[0] as VersionAttribute;
+            Console.WriteLine("Version of this class: {0}", attr.Version);
+        }
+    }
+}
---
+++ C# OOP/02.DefiningClassesPart2/04.VersionAttribute/VersionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.VersionAttribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct |
+        AttributeTargets.Interface | AttributeTargets.Enum | AttributeTargets.Method)]
+    public class VersionAttribute : Attribute
+    {
+        public string Version { get; set; }
+
+        public VersionAttribute(string version)
+        {
+            this.Version = version;
+        }
+    }
+}
