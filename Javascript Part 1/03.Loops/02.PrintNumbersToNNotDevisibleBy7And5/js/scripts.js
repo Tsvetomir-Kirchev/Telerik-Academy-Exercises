452fbd12bdbe99d5f51d82b02434e662280fd41e
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-10-10 13:49:32 +0300

    [Javascript Part 1]: Added PrintNumbersToNNotDevisibleBy7And5

---
+++ Javascript Part 1/03.Loops/02.PrintNumbersToNNotDevisibleBy7And5/js/scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+	document.getElementById("btnPrintNumbers").addEventListener("click", printNumbers);
+}
+
+function printNumbers() {
+	var number = parseInt(document.getElementById("number").value);
+	if(number == Number(number)) {
+		var result = ""
+		for(var i = 0; i <= number; i++) {
+			if((i % 7 == 0) && (i % 5 == 0)) {
+				continue;
+			}
+			result += (i + " ");
+		}
+		showMessage(result);
+	} else {
+		showMessage("PLease enter a number");
+	}
+}
+
+function showMessage(msg) {
+	document.getElementById("result").innerText = msg;
+}
