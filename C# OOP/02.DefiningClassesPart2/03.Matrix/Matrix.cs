d83aaf495975d5a586d8219d44804006902981f8
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-10-10 17:51:33 +0300

    [C# OOP]: Added Matrix

---
+++ C# OOP/02.DefiningClassesPart2/03.Matrix/Matrix.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Matrix
+{
+    public class Matrix<T>
+    {
+        private T[,] matrix;
+        private int rows;
+        private int cols;
+
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            this.matrix = new T[rows, cols];
+        }
+
+        public T this[int row, int col]
+        {
+            get
+            {
+                if ((row >= rows || row < 0) || (col >= cols || col < 0))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return matrix[col, row];
+            }
+            set
+            {
+                if ((row >= rows || row < 0) || (col >= cols || col < 0))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                this.matrix[row, col] = value;
+            }
+        }
+
+        public static Matrix<T> operator +(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            if ((matrix1.rows != matrix2.rows) || (matrix2.cols != matrix1.cols))
+            {
+                throw new ArgumentException("The matricies are not equal");
+            }
+            Matrix<T> resultMatrix = new Matrix<T>(matrix1.rows, matrix1.cols);
+            for (int i = 0; i < matrix1.rows; i++)
+            {
+                for (int j = 0; j < matrix1.cols; j++)
+                {
+                    dynamic m1 = matrix1[i, j];
+                    dynamic m2 = matrix2[i, j];
+                    resultMatrix[i, j] = m1 + m2;
+                }
+            }
+            return resultMatrix;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            if ((matrix1.rows != matrix2.rows) || (matrix2.cols != matrix1.cols))
+            {
+                throw new ArgumentException("The matricies are not equal");
+            }
+            Matrix<T> resultMatrix = new Matrix<T>(matrix1.rows, matrix1.cols);
+            for (int i = 0; i < matrix1.rows; i++)
+            {
+                for (int j = 0; j < matrix1.cols; j++)
+                {
+                    dynamic m1 = matrix1[i, j];
+                    dynamic m2 = matrix2[i, j];
+                    resultMatrix[i, j] = m1 - m2;
+                }
+            }
+            return resultMatrix;
+        }
+
+        public static Matrix<T> operator *(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            if (matrix1.cols != matrix2.rows)
+            {
+                throw new ArgumentException("The cols of first matrix and rows of second matrix are not equal");
+            }
+            Matrix<T> resultMatrix = new Matrix<T>(matrix1.cols, matrix1.rows);
+            for (int i = 0; i < matrix1.cols; i++)
+            {
+                for (int j = 0; j < matrix2.rows; j++)
+                {
+                    dynamic m1 = matrix1[i, j];
+                    dynamic m2 = matrix2[j, i];
+                    resultMatrix[i, j] = m1 * m2;
+                }
+            }
+            return resultMatrix;
+        }
+
+        public static bool operator true(Matrix<T> matrix)
+        {
+            bool result = true;
+            for (int i = 0; i < matrix.rows; i++)
+            {
+                for (int j = 0; j < matrix.cols; j++)
+                {
+                    if (matrix[i, j] == (dynamic)0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static bool operator false(Matrix<T> matrix)
+        {
+            bool result = false;
+            for (int i = 0; i < matrix.rows; i++)
+            {
+                for (int j = 0; j < matrix.cols; j++)
+                {
+                    if (matrix[i, j] != (dynamic)0)
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    sb.Append(matrix[row, col]);
+                    sb.Append("    ");
+                }
+                sb.AppendLine();
+            }
+            return (sb.ToString());
+        }
+    }
+}
---
+++ C# OOP/02.DefiningClassesPart2/03.Matrix/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> matrix1 = new Matrix<int>(5, 5);
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    matrix1[i, j] = i * j;
+                }
+            }
+
+            Matrix<int> matrix2 = new Matrix<int>(5, 5);
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    matrix2[i, j] = i * j;
+                }
+            }
+
+            int el = matrix1[2, 2];
+            Console.WriteLine("matrix1[2, 2]: {0}", el);
+            Console.WriteLine("matrix1:");
+            Console.WriteLine(matrix1.ToString());
+            Console.WriteLine("matrix2: ");
+            Console.WriteLine(matrix2.ToString());
+
+            Matrix<int> matrixMultiplied = new Matrix<int>(5, 5);
+            matrixMultiplied = matrix1 * matrix2;
+            Console.WriteLine("matrix1 * matrix2:");
+            Console.WriteLine(matrixMultiplied.ToString());
+
+            Matrix<int> matrixAddition = new Matrix<int>(5, 5);
+            matrixAddition = matrix1 + matrix2;
+            Console.WriteLine("matrix1 + matrix2:");
+            Console.WriteLine(matrixAddition.ToString());
+
+            Matrix<int> matrixSubtract = new Matrix<int>(5, 5);
+            matrixAddition = matrix1 + matrix2;
+            Console.WriteLine("matrix1 - matrix2:");
+            Console.WriteLine(matrixSubtract.ToString());
+
+            Matrix<int> m = new Matrix<int>(2, 2);
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    m[i, j] = 1;
+                }
+            }
+            if (m)
+            {
+                Console.WriteLine("m have no element with value 0");
+            }
+        }
+    }
+}
