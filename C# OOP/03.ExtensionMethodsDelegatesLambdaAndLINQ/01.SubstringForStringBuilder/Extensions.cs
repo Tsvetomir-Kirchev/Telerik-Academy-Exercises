c0e9afd54766993ad847161a53168c8870d92d8b
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2014-01-04 11:28:16 +0200

    [C# OOP]: Added SubstringFirStringBuilder

---
+++ C# OOP/03.ExtensionMethodsDelegatesLambdaAndLINQ/01.SubstringForStringBuilder/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SubstringForStringBuilder
+{
+    public static class Extensions
+    {
+        public static StringBuilder Substring(this StringBuilder sb, int index, int length)
+        {
+            StringBuilder result = new StringBuilder();
+            if (index < 0 || index > sb.Length
+                || length > sb.Length || index + length > sb.Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            for (int i = index; i < length; i++)
+            {
+                result.Append(sb[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ C# OOP/03.ExtensionMethodsDelegatesLambdaAndLINQ/01.SubstringForStringBuilder/SubstringFirStringBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SubstringForStringBuilder
+{
+    class SubstringFirStringBuilder
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hello, how are you?");
+            Console.WriteLine(sb.Substring(7, 10));
+        }
+    }
+}
